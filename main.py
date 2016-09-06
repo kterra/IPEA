99b046e3724958c1a018affbdb482c3d88c4a2e7
Kizzy Fernanda Terra Ferreira dos Reis <kizzyterra@gmail.com>
2016-09-06 14:42:12 -0300

    adding files

---
+++ main.py
@@ -0,0 +1,22 @@
+from preprocessing import *
+from classifier import *
+import os
+
+SOURCE_PMB = "PMB"
+SOURCE_SAMMED = "SAMMED"
+
+
+if __name__ == '__main__':
+    print("Start...")
+
+    # load_dcb_table_into_memory()
+    # pre_processing_table("FAT_QTD_PMB_PPP_0310_0215_999_FORMATADA.XLS","PMB", 1,2,3,4)
+    # all_filenames = os.listdir("raw")
+    # all_filenames = [filename for filename in all_filenames if filename.startswith("rel_LISTA_REVISTA_SAMMED")]
+    # for fname in all_filenames:
+    #    pre_processing_table(fname,"SAMMED",8,7,6,3)
+
+    product_matches(SOURCE_PMB, SOURCE_SAMMED)
+
+
+    print("Finished.")
